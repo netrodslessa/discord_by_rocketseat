4363e202f968a9e4749d7d91477fba5c7d0b45d1
netrodslessa <netrods@gmail.com>
2020-06-18 17:34:49 -0300

    iniciado o segundo grid

--- clone-discord-rocket/src/components/Layout/index.tsx
+++ clone-discord-rocket/src/components/Layout/index.tsx
@@ -4,13 +4,16 @@ import { Grid } from './styles';
 
 import ServerList from '../ServerList';
 
+import ServerName from '../ServerName';
+
 
 const Layout: React.FC = () => {
     return(
         <Grid>
             <ServerList />
+            <ServerName />
         </Grid>
-    )
-}
+    );
+};
 
 export default Layout;
