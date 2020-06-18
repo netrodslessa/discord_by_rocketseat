46563d9460e27f2fb8b2e20cd6bfde33fc8aad6d
netrodslessa <netrods@gmail.com>
2020-06-18 17:06:20 -0300

    finalizado primeiro grid

--- clone-discord-rocket/src/App.tsx
+++ clone-discord-rocket/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
+import Layout from './components/Layout';
 import GlobalStyles from './styles/globalStyles';
 
 function App() {
   return (
     <>
-    <h1>Teste</h1>
+    <Layout />
 
     <GlobalStyles />
     </>
