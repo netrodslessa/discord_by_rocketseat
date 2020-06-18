481451d230e0353351c5d7fbf248bfa78658ce41
netrodslessa <netrods@gmail.com>
2020-06-18 14:59:55 -0300

    Configurado eslint

---
+++ clone-discord-rocket/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import GlobalStyles from './styles/globalStyles';
+
+function App() {
+  return (
+    <>
+    <h1>Teste</h1>
+
+    <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
