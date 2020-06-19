b9493119c675d82bb597a2e9e28a07f960a3b1df
netrodslessa <netrods@gmail.com>
2020-06-19 14:19:34 -0300

    após ter feito toda a parte do UserList, apaguei sem querer, agora vou iniciar novamente o CSS do mesmo

--- clone-discord-rocket/src/components/Layout/index.tsx
+++ clone-discord-rocket/src/components/Layout/index.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 
 import { Grid } from './styles';
-
 import ServerList from '../ServerList';
-
 import ServerName from '../ServerName';
-
+import ChannelInfo from '../ChannelInfo';
+import ChannelList from '../ChannelList';
+import UserInfo from '../UserInfo';
+import UserList from '../UserList';
 
 const Layout: React.FC = () => {
     return(
         <Grid>
             <ServerList />
             <ServerName />
+            <ChannelInfo />
+            <ChannelList />
+            <UserInfo />
+            <UserInfo />
+            <div />
+            <UserList />
         </Grid>
     );
 };
